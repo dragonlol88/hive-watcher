fdf58bcf101e1ec7faa1c4d572770a5e4b20cf03
suuny <uram24@42maru.com>
2021-03-25 11:56:13 +0900

    Move server

---
+++ tests/test_emitter.py
@@ -0,0 +1,103 @@
+import os
+import time
+import queue
+import shutil
+import pytest
+
+from watcher import EventStatus
+from tests import run_test_watcher, run_test_server
+
+
+watcher = None
+client  = None
+STORED_DIR = '/tmp/watcher'
+
+
+@pytest.fixture
+def run_watcher(root_dir, ignore_pattern, proj_depth, host, port):
+    return run_test_watcher(root_dir, ignore_pattern, proj_depth, host, port)
+
+
+def get_client(client_address, watcher_address):
+    return run_test_server(client_address, watcher_address)
+
+
+def get_entries(stored_dir):
+    entries = []
+    for entry in os.scandir(stored_dir):
+        entries.append(entry)
+    return entries
+
+
+def pair_of_origins(entries, origin_text):
+    pair_of_origins = []
+
+    for entry in entries:
+        with open(entry.path, 'r') as f:
+            keywords = [line.strip() for line in f.readlines()]
+            pair_of_origins.append((origin_text[os.path.basename(entry.path)],
+                                    keywords))
+    return pair_of_origins
+
+
+def test_file_created(run_watcher, event_num):
+    global watcher
+
+    watcher = run_watcher
+    timeout = 2
+
+    events = []
+    while True:
+        try:
+            event = watcher.get_event(timeout)
+            events.append(event)
+        except queue.Empty:
+            break
+
+    assert event_num == len(events)
+
+    for i in range(event_num):
+        event = events[i]
+        symbol = event.symbol
+        assert symbol.event_type == EventStatus.FILE_CREATED
+
+
+def test_channel_created(
+        client_address, watcher_address,stored_directory, event_num
+):
+    global watcher
+    global client
+    sleep_time = 5
+    client = get_client(client_address, watcher_address)
+    time.sleep(sleep_time)
+    entries = get_entries(stored_directory)
+    event = watcher.get_event()
+    watch = event.watch
+    channel = watch.channels.pop()
+    assert channel.split("/")[-1] == ':'.join([str(addr) for addr in client_address])
+    assert len(entries) == event_num
+
+
+def test_created_files(stored_directory, origin_text):
+
+    entries = get_entries(stored_directory)
+    pairs = pair_of_origins(entries, origin_text)
+
+    for origin, keywords in pairs:
+        assert origin == keywords
+    shutil.rmtree(stored_directory)
+
+
+def test_deleted_channel():
+    global watcher
+    global client
+
+    client.close()
+    event = watcher.get_event()
+    watch = event.watch
+    symbol = event.symbol
+    assert not watch.channels
+    assert symbol.event_type == EventStatus.DELETE_CHANNEL
+
+
+
