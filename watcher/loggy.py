d5fd6005e6d1e06fe758d8b0de2269861b1b18ff
suuny <uram24@42maru.com>
2021-04-05 21:05:09 +0900

    Initial commit

---
+++ watcher/loggy.py
@@ -0,0 +1,138 @@
+import sys
+import logging
+import logging.config
+
+LOG_LEVELS = {
+    "critical": logging.CRITICAL,
+    "error": logging.ERROR,
+    "warning": logging.WARNING,
+    "info": logging.INFO,
+    "debug": logging.DEBUG
+}
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "watcher.loggy.DefaultFormatter",
+            "fmt": "%(asctime)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)",
+            "datefmt": '%m/%d/%Y %I:%M:%S %p',
+            "use_colors": True,
+        },
+        "file": {
+            "()": "watcher.loggy.FileFormatter",
+            "fmt": "%(asctime)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)",
+            "datefmt": '%m/%d/%Y %I:%M:%S %p',
+            "use_colors": False,
+        },
+
+    },
+    "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+        "logfile":
+            {
+                "formatter": "file",
+                "class": "logging.FileHandler",
+                "filename": "watcher.log"
+            }
+    },
+    "loggers": {
+        "watcher": {"handlers": ["default"], "level": "INFO"}
+    }
+}
+
+COLORS = {
+        'grey': "\x1b[38;21m",
+        'yellow': "\x1b[33;21m",
+        'red': "\x1b[31;21m",
+        'bold_red': "\x1b[31;1m",
+
+    }
+RESET = "\x1b[0m"
+
+
+class ColoredFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors"""
+
+    FORMATS_COLOR = {
+        logging.DEBUG: COLORS['grey'],
+        logging.INFO: COLORS['grey'],
+        logging.WARNING: COLORS['yellow'],
+        logging.ERROR: COLORS['red'],
+        logging.CRITICAL: COLORS['bold_red']
+    }
+
+    def __init__(self, fmt=None, datefmt=None, style="%", use_colors=None):
+
+        if use_colors:
+            self.use_colors = use_colors
+        else:
+            self.use_colors = sys.stdout.isatty()
+
+        super().__init__(fmt=fmt, datefmt=datefmt, style=style)
+
+    def set_colored_fmt(self, record):
+        color = self.FORMATS_COLOR[record.levelno]
+        self._style._fmt = color + self._fmt + RESET
+
+    def _custom_record(self, record):
+        raise NotImplementedError
+
+    def custom_record(self, record):
+
+        if self.use_colors and not hasattr(record, 'color'):
+            self.set_colored_fmt(record)
+        record = self._custom_record(record)
+        return record
+
+    def formatMessage(self, record) -> str:
+        record = self.custom_record(record)
+        return super().formatMessage(record)
+
+
+class DefaultFormatter(ColoredFormatter):
+
+    def _custom_record(self, record):
+        color = record.__dict__.get('color', None)
+        if color:
+            self._style._fmt = COLORS[color] + self._fmt + RESET
+        return record
+
+
+class FileFormatter(ColoredFormatter):
+
+    def _custom_record(self, record):
+        return record
+
+
+class EventFormatter(ColoredFormatter):
+
+    def _custom_record(self, record):
+        pass
+
+
+def configure_logging():
+    logging.config.dictConfig(LOGGING_CONFIG)
+
+
+# # create logger with 'spam_application'
+# logger = logging.getLogger("My_app")
+# logger.setLevel(logging.DEBUG)
+#
+# # create console handler with a higher log level
+# ch = logging.StreamHandler()
+# ch.setLevel(logging.DEBUG)
+#
+# ch.setFormatter(ColoredFormatter(fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"))
+#
+# logger.addHandler(ch)
+# logger.debug("debug message")
+# logger.info("info message")
+# logger.warning("warning message")
+# logger.error("error message")
+# logger.critical("critical message")
